9e3cbe38780624119e5d678a0ea4b5efc1f738a1
Chris Kite <chris@chriskite.com>
2016-03-29 17:34:44 -0500

    run only as a forwarder

--- 01_bootstrap_djbdns.sh
+++ 01_bootstrap_djbdns.sh
@@ -1,18 +1,19 @@
 #!/bin/sh 
 set -e
 
-if [ -z "$CACHE_IP" ]; then echo "CACHE_IP is not set" && exit 1; fi
-
 #
 # Setup djbdns as a network cache: http://cr.yp.to/djbdns/run-cache-x.html
 #
 useradd Gdnscache && useradd Gdnslog
-dnscache-conf Gdnscache Gdnslog /etc/dnscache $CACHE_IP
+dnscache-conf Gdnscache Gdnslog /etc/dnscache 0.0.0.0
 
 # Allow only private networks to access the DNS server
 touch /etc/dnscache/root/ip/10
 for i in `seq 16 31`; do touch /etc/dnscache/root/ip/172.$i; done
 touch /etc/dnscache/root/ip/192.168
+
+# Run dnscache only as a forwarder
+echo 1 > /etc/dnscache/env/FORWARDONLY
 
 # Put the contents of /etc/resolv.conf into the dnscache server list
 /usr/local/bin/migrate_resolvconf.py
